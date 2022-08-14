073068e291a18b7e6c0e0115f076695cfd754313
veronika-sid1 <111087580+veronika-sid1@users.noreply.github.com>
2022-08-14 19:45:39 +0300

    Созданы классы. объекты по задаче из видоматериала

--- sandbox/src/main/java/ru/stqa/pft/sandbox/first.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/first.java
@@ -3,7 +3,24 @@ package ru.stqa.pft.sandbox;
 public class first {
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    hello("world");
+    hello("user");
+
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площвть прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+  }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 
+  public static double area(Square s) {
+    return s.l * s.l;
+  }
+
+  public static double area(Rectangle r) {
+    return r.a * r.b;
+  }
 }
