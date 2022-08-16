44a63d570547747c04d6d546fa7681fa90c08490
veronika-sid1 <111087580+veronika-sid1@users.noreply.github.com>
2022-08-16 20:51:59 +0300

    Все что было в видео + создан тестовый класс

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -7,21 +7,14 @@ public class First {
     hello("user");
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
-  }
 }
