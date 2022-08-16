ea24004941b9b7c06a5032e7c8aacd25a750cd22
veronika-sid1 <111087580+veronika-sid1@users.noreply.github.com>
2022-08-16 17:55:23 +0300

    Создан метод и в запускаемый класс добавлены примеры использования метода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,24 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 
   public static double distance(Point p1, Point p2) {
-    double dist = Math.sqrt((Math.pow((p2.x2 - p1.x1), 2)) + Math.pow((p2.y2 - p1.y1), 2));
+    double dist = Math.sqrt((Math.pow((p2.x - p1.x), 2)) + Math.pow((p2.y - p1.y), 2));
     return dist;
   }
 
   public static void main(String[] args) {
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x1 = 5;
-    p1.y1 = 2;
-    p2.x2 = 10;
-    p2.y2 = 4;
-    System.out.println("Расстояние между точками (" + p1.x1 + "," + p1.y1 + ") и (" + p2.x2 + "," + p2.y2 + ") = " + distance(p1, p2));
+    Point point1 = new Point(5,2);
+    Point point2 = new Point(10,4);
+    System.out.println(Point.distance(point1,point2));
+    Point point3 = new Point(12,34);
+    Point point4 = new Point(-1,4);
+    System.out.println(Point.distance(point3,point4));
   }
 
 }
