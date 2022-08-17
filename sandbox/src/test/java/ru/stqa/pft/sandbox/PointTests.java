621d99c5fca6827551efbaa71f84e9c832c4838e
veronika-sid1 <111087580+veronika-sid1@users.noreply.github.com>
2022-08-17 16:42:05 +0300

    Тесты разделены, убрана лишняя проверка

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,12 +9,15 @@ public class PointTests {
   public void testPoint() {
     Point point1 = new Point(5,2);
     Point point2 = new Point(10,4);
+    Assert.assertEquals(point1.distance(point2),5.385164807134504);
+    Assert.assertTrue(point1.distance(point2) > 2);
+  }
+
+  @Test
+  public void testPoint1() {
     Point point3 = new Point(12,34);
     Point point4 = new Point(-1,4);
-    Assert.assertEquals(Point.distance(point1,point2),5.385164807134504);
-    Assert.assertEquals(Point.distance(point3,point4),32.69556544854363);
-    Assert.assertNotSame(Point.distance(point1,point2), Point.distance(point3,point4));
-    Assert.assertTrue(Point.distance(point1,point2) > 2);
-    Assert.assertFalse(Point.distance(point3,point4) == 0);
+    Assert.assertEquals(point3.distance(point4),32.69556544854363);
+    Assert.assertFalse(point3.distance(point4) == 0);
   }
 }
