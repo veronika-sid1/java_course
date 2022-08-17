a8aa0c89279c2afea25950e566ce1934cfee65d6
veronika-sid1 <111087580+veronika-sid1@users.noreply.github.com>
2022-08-17 16:37:22 +0300

    Внесены правки по комментарию
    убран static у метода, передаваемый параметр теперь один, изменен вызов метода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -10,18 +10,18 @@ public class Point {
     this.y = y;
   }
 
-  public static double distance(Point p1, Point p2) {
-    double dist = Math.sqrt((Math.pow((p2.x - p1.x), 2)) + Math.pow((p2.y - p1.y), 2));
+  public double distance(Point p2) {
+    double dist = Math.sqrt((Math.pow((p2.x - this.x), 2)) + Math.pow((p2.y - this.y), 2));
     return dist;
   }
 
   public static void main(String[] args) {
     Point point1 = new Point(5,2);
     Point point2 = new Point(10,4);
-    System.out.println("Расстояние между точками (" + point1.x + "," + point1.y + ") и (" + point2.x + "," + point2.y + ") = " + Point.distance(point1,point2));
+    System.out.println("Расстояние между точками (" + point1.x + "," + point1.y + ") и (" + point2.x + "," + point2.y + ") = " + point1.distance(point2));
     Point point3 = new Point(12,34);
     Point point4 = new Point(-1,4);
-    System.out.println("Расстояние между точками (" + point3.x + "," + point3.y + ") и (" + point4.x + "," + point4.y + ") = " + Point.distance(point3,point4));
+    System.out.println("Расстояние между точками (" + point3.x + "," + point3.y + ") и (" + point4.x + "," + point4.y + ") = " + point3.distance(point4));
   }
 
 }
