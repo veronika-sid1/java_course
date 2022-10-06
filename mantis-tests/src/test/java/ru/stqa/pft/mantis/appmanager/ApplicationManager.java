f263364e2511497348e496c8203f3e70bf8af780
veronika-sid1 <111087580+veronika-sid1@users.noreply.github.com>
2022-10-06 16:39:52 +0300

    Подготовлен новый проект для Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,48 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ApplicationManager {
+
+  private final Properties properties;
+  WebDriver wd;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  public void stop() {
+    logout();
+    wd.quit();
+  }
+}
